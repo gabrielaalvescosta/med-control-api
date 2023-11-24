a3e0fb61f333c0182f38cd5a4259126188f87688
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2023-11-24 03:44:18 -0300

    Configuracao swagger

--- src/test/java/com/medcontrol/medcontrol/MedcontrolApplicationTests.java
+++ src/test/java/com/medcontrol/medcontrol/MedcontrolApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MedcontrolApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
