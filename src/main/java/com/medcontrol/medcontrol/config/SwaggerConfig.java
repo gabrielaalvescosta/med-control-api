4ce0d6ba2331fabe628c6e28894d4c4fefc4b6d2
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2023-11-24 03:17:16 -0300

    Configuracao swagger

--- src/main/java/com/medcontrol/medcontrol/config/SwaggerConfig.java
+++ src/main/java/com/medcontrol/medcontrol/config/SwaggerConfig.java
@@ -1,46 +1,33 @@
 package com.medcontrol.medcontrol.config;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.service.Contact;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//import static springfox.documentation.builders.PathSelectors.regex;
-//
-//@Configuration
-//@EnableSwagger2
-//public class SwaggerConfig extends WebMvcConfigurationSupport {
-//    @Bean
-//    public Docket productApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("guru.springframework.controllers"))
-//                .paths(regex("/product.*"))
-//                .build()
-//                .apiInfo(metaData());
-//    }
-//    private ApiInfo metaData() {
-//        return new ApiInfoBuilder()
-//                .title("Spring Boot REST API")
-//                .description("\"Spring Boot REST API for Online Store\"")
-//                .version("1.0.0")
-//                .license("Apache License Version 2.0")
-//                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
-//                .contact(new Contact("John Thompson", "https://springframework.guru/about/", "john@springfrmework.guru"))
-//                .build();
-//    }
-//    @Override
-//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("swagger-ui.html")
-//                .addResourceLocations("classpath:/META-INF/resources/");
-//
-//        registry.addResourceHandler("/webjars/**")
-//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-//    }
-//}
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("MED CONTROL API")
+                        .description("Api do sistema de controle de medicamentos medcontrol.")
+                        .summary("Api Java feita PARA solucionar a aquisição de produtos para o estoque do farmacêutico")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("GABRIELA COSTA")
+                                .email("rm96051@fiap.com.br")
+                        )
+                        .license(new License()
+                                .name("MIT Open Soucer")
+                                .url("http://github.com/gabrielaalvescosta")
+                        )
+                );
+}
+}
