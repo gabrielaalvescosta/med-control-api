65f6fd00f1d18be68021f927281778a4217e4934
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2023-11-24 05:33:27 -0300

    Configuracao swagger

--- src/main/java/com/medcontrol/medcontrol/MedcontrolApplication.java
+++ src/main/java/com/medcontrol/medcontrol/MedcontrolApplication.java
@@ -2,9 +2,10 @@ package com.medcontrol.medcontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
+@ComponentScan(basePackages = "com.medcontrol.medcontrol")
 public class MedcontrolApplication {
 
 	public static void main(String[] args) {
